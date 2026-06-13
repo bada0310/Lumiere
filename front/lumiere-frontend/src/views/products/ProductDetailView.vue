3319f9dfe339dcea36ca940b2ddbb325e806125f
bada0310 <pppp000310@gmail.com>
2026-06-14 07:55:03 +0900

    ♻️[refactor]: App.vue 수정 및 import 수정

---
+++ front/lumiere-frontend/src/views/products/ProductDetailView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>제품 상세 페이지 (Product Detail)</h1>
+  </div>
+</template>
+
+<script setup>
+// 향후 여기에 컴포넌트 로직을 작성하시면 됩니다.
+</script>
+
+<style scoped>
+/* 컴포넌트 전용 스타일을 작성하는 곳입니다. */
+</style>
