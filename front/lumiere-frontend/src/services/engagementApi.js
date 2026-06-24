22529f8a99f7a68139462119344ad7fa95c10e32
bada0310 <pppp000310@gmail.com>
2026-06-24 15:37:56 +0900

    ♻️ [refactor]: api 수정

--- front/lumiere-frontend/src/services/engagementApi.js
+++ front/lumiere-frontend/src/services/engagementApi.js
@@ -34,31 +34,3 @@ export const deleteLikedProductOption = async (id) => {
     headers: authHeaders(),
   })
 }
-
-export const getUrlAnalysisRecords = async (params = {}) => {
-  const response = await axios.get(`${API_BASE_URL}/api/engagements/url-analyses/`, {
-    headers: authHeaders(),
-    params,
-  })
-  return response.data
-}
-
-export const getUrlAnalysisRecord = async (id) => {
-  const response = await axios.get(`${API_BASE_URL}/api/engagements/url-analyses/${id}/`, {
-    headers: authHeaders(),
-  })
-  return response.data
-}
-
-export const createUrlAnalysisRecord = async (payload) => {
-  const response = await axios.post(`${API_BASE_URL}/api/engagements/url-analyses/`, payload, {
-    headers: authHeaders(),
-  })
-  return response.data
-}
-
-export const deleteUrlAnalysisRecord = async (id) => {
-  await axios.delete(`${API_BASE_URL}/api/engagements/url-analyses/${id}/`, {
-    headers: authHeaders(),
-  })
-}
